798a9542ed4b095512877145bfc1d2011372af3b
Oleg <88669750+spooll@users.noreply.github.com>
2024-03-20 15:19:56 +0300

    Update Exch-Maintenance.ps1

--- Exch-Maintenance.ps1
+++ Exch-Maintenance.ps1
@@ -1,10 +1,11 @@
-﻿<#
+function global:Exch-Maintenance {
+<#
 .SYNOPSIS
     Function to start and stop Exchange Server Maintenance.
 .DESCRIPTION
     You don`t need edit this script, but use it in local Exchange Server session.
-    Its total 70-lines function script instead of 3 572-lines scripts by MS (with skipped Signature lines): 
-    StartDagServerMaintenance.ps1, StopDagServerMaintenance.ps1, RedistributeActiveDatabases.ps1, 
+    Its total 70-lines function script instead of 3 572-lines scripts by MS (with skipped Sig.): 
+    StartDagServerMaintenance, StopDagServerMaintenance, RedistributeActiveDatabases, 
     which are difficult and lazy to parse (has anyone figured out what's inside?).
 
     For your convenience, it is recommended to write the function in the profile file,
@@ -31,49 +32,46 @@
     Exch-Maintenance -Start
     Exch-Maintenance -Stop
 #>
-function global:Exch-Maintenance {
-
     param(
-        #[Parameter(ParameterSetName="Start")]
         [switch]$Start,
-        #[Parameter(ParameterSetName="Stop")]
         [switch]$Stop
     )
 
     $WarningActionPreference = "SilentlyContinue"
-    if ($Start)
-    {
+    while (-Not(Get-PSSession|Where-Object ConfigurationName -eq "Microsoft.Exchange")) {
+        $exch= (Get-ADComputer -Filter "name -like 's-ex-0*'").name| Get-Random
+        $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$exch/Powershell" -Authentication Kerberos
+        Import-PSSession $session -DisableNameChecking -AllowClobber | out-null
+        Write-Host Exchange PSSession loaded successfully! -ForegroundColor magenta
+    }
+    if ($Start){
         Set-ServerComponentState $env:COMPUTERNAME -Component HubTransport -State Draining -Requester Maintenance
         Set-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline -State InActive -Requester Maintenance
         Redirect-Message -Server $env:COMPUTERNAME -Target (get-exchangeserver |Where-Object Name -ne $env:COMPUTERNAME| Get-Random).fqdn -Confirm:$false -ErrorAction SilentlyContinue
         Suspend-ClusterNode -Name $env:COMPUTERNAME -ErrorAction silentlycontinue
         Set-MailboxServer $env:COMPUTERNAME -DatabaseCopyActivationDisabledAndMoveNow $true
         Set-MailboxServer $env:COMPUTERNAME -DatabaseCopyAutoActivationPolicy Blocked
-        while (Get-MailboxDatabaseCopyStatus | Where-Object status -eq "mounted")
-        {
-            foreach ($mb in (Get-MailboxDatabaseCopyStatus | Where-Object status -eq "mounted")) 
-            {
+        while (Get-MailboxDatabaseCopyStatus | Where-Object status -eq "mounted"){
+            foreach ($mb in (Get-MailboxDatabaseCopyStatus | Where-Object status -eq "mounted")) {
                 Move-ActiveMailboxDatabase -Server $env:COMPUTERNAME -Confirm:$false
             }
         }
         ""
-        Write-Host "$env:COMPUTERNAME ComponentState HubTransport is" (Get-ServerComponentState $env:COMPUTERNAME -Component HubTransport).State "(Should Draining)"
+        Write-Host "$env:COMPUTERNAME ComponentState HubTransport is" (Get-ServerComponentState $env:COMPUTERNAME -Component HubTransport).State "(Should Inactive)"
         Write-Host "$env:COMPUTERNAME ComponentState ServerWideOffline is" (Get-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline).state "(Should InActive)" 
         Write-Host "$env:COMPUTERNAME DatabaseCopyActivationDisabledAndMoveNow is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyActivationDisabledAndMoveNow "(Should True)"
         Write-Host "$env:COMPUTERNAME DatabaseCopyAutoActivationPolicy is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyAutoActivationPolicy "(Should Blocked)"
         ""
-        if ($DB=(Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME | Where-Object Status -eq "Mounted").name)
-        {
+        if ($DB=(Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME | Where-Object Status -eq "Mounted").name){
             Write-Host "You Have Some Bases Mounted!!! $DB" -ForeGround RED
         }
-        Else 
-        {
+        Else {
             Write-Host "You Have No Bases Mounted!!!" -ForeGround GREEN
         }
     }
     
-    if ($Stop)
-    {
+    if ($Stop){
+        $Missed=Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME  |Where-Object {($_.Status -ne "Mounted") -and ($_.ActivationPreference -eq 1)}
         Write-Host "Prepearing to activate server components" -Foreground GREEN
         Set-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline -State Active -Requester Maintenance
         Resume-ClusterNode -Name $env:COMPUTERNAME -ErrorAction silentlycontinue
@@ -86,12 +84,20 @@ function global:Exch-Maintenance {
         Write-Host "$env:COMPUTERNAME ComponentState ServerWideOffline is" (Get-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline).state "(Should Active)"
         Write-Host "$env:COMPUTERNAME DatabaseCopyActivationDisabledAndMoveNow is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyActivationDisabledAndMoveNow "(Should False)"
         Write-Host "$env:COMPUTERNAME DatabaseCopyAutoActivationPolicy is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyAutoActivationPolicy "(Should Unrestricted)"
-        Get-MailboxDatabaseCopyStatus * -Active | Format-Table Name, Status, MailboxServer,ActivationPreference
-        Write-Host "Prepearing to switch back Active Databases" -Foreground GREEN
-        Start-Sleep 5
-        foreach ($mbx in (Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME  |Where-Object {($_.Status -ne "Mounted") -and ($_.ActivationPreference -eq 1)}| Select-Object -ExpandProperty DatabaseName))
-        {
-            Move-ActiveMailboxDatabase -SkipAllChecks -Identity $mbx -ActivateOnServer $env:COMPUTERNAME -Confirm:$false
+        if ($Missed){
+            Get-MailboxDatabaseCopyStatus * -Active | Format-Table Name, Status, MailboxServer,ActivationPreference
+            Write-Host "Prepearing to switch back Active Databases" -Foreground GREEN
+            Start-Sleep 5
+            while (
+                $Queue=Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME  |Where-Object ReplayQueueLength -gt 10){
+                    Write-Host "Waiting for ReplayQueueLength" -ForegroundColor Yellow
+                    $Queue| Select-Object Name, ReplayQueueLength
+                    Start-Sleep 10
+                }
+            foreach ($mbx in $Missed.DatabaseName)
+            {
+                Move-ActiveMailboxDatabase -SkipAllChecks -Identity $mbx -ActivateOnServer $env:COMPUTERNAME -Confirm:$false
+            }
         }
         Get-MailboxDatabaseCopyStatus * -Active | Format-Table Name, Status, MailboxServer, ActivationPreference
     }
