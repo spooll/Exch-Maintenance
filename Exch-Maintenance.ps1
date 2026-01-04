9e54df4b91c51461e9dbaae97ad6722e8f3b16a3
Oleg <88669750+spooll@users.noreply.github.com>
2026-01-04 21:50:25 +0300

    Big Update!
    Now script reworked to run remotely!

--- Exch-Maintenance.ps1
+++ Exch-Maintenance.ps1
@@ -1,71 +1,69 @@
-function global:Exch-Maintenance {
-
+function global:Exch-Maintenance {    
     param(
-        [switch]$Start,
-        [switch]$Stop
+        [Parameter(ParameterSetName='START',Mandatory=$true)][switch]$Start,
+        [Parameter(ParameterSetName='STOP',Mandatory=$true)][switch]$Stop,
+        [Parameter(Mandatory)][ValidateSet("s-ex-01","s-ex-02","s-ex-03","s-ex-04","s-ex-05")]$Exch
     )
 
     $WarningActionPreference = "SilentlyContinue"
-    while (-Not(Get-PSSession|Where-Object ConfigurationName -eq "Microsoft.Exchange")) {
-        $exch= (Get-ADComputer -Filter "name -like 's-exch-0*'").name| Get-Random                                            #Put your Exchange server names template here!!!
-        $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$exch/Powershell" -Authentication Kerberos
-        Import-PSSession $session -DisableNameChecking -AllowClobber | out-null
-        Write-Host Exchange PSSession loaded successfully! -ForegroundColor magenta
+    $count=0
+    while ((-Not(Get-PSSession|Where-Object ConfigurationName -eq "Microsoft.Exchange")) -and ($count -ne "5")) {
+        $count++
+        $exch1= (Get-ADComputer -Filter "name -like 's-ex-0*'").name| Get-Random
+        try{
+            Write-Host Trying to connect $exch1...
+            $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$exch1/Powershell" -Authentication Kerberos -ErrorAction Stop
+            Import-PSSession $session -DisableNameChecking -AllowClobber -ErrorAction Stop | out-null
+            Write-Host Exchange PSSession loaded successfully! -ForegroundColor magenta
+        }
+        Catch{$Error[0].Exception.Message }
     }
     if ($Start){
-        Set-ServerComponentState $env:COMPUTERNAME -Component HubTransport -State Draining -Requester Maintenance
-        Set-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline -State InActive -Requester Maintenance
-        Redirect-Message -Server $env:COMPUTERNAME -Target (get-exchangeserver |Where-Object Name -ne $env:COMPUTERNAME| Get-Random).fqdn -Confirm:$false -ErrorAction SilentlyContinue
-        Suspend-ClusterNode -Name $env:COMPUTERNAME -ErrorAction silentlycontinue
-        Set-MailboxServer $env:COMPUTERNAME -DatabaseCopyActivationDisabledAndMoveNow $true
-        Set-MailboxServer $env:COMPUTERNAME -DatabaseCopyAutoActivationPolicy Blocked
-        while (Get-MailboxDatabaseCopyStatus | Where-Object status -eq "mounted"){
-            foreach ($mb in (Get-MailboxDatabaseCopyStatus | Where-Object status -eq "mounted")) {
-                Move-ActiveMailboxDatabase -Server $env:COMPUTERNAME -Confirm:$false
-            }
-        }
+        Set-ServerComponentState $Exch -Component HubTransport -State Draining -Requester Maintenance
+        Set-ServerComponentState $Exch -Component ServerWideOffline -State InActive -Requester Maintenance
+        Redirect-Message -Server $Exch -Target (get-exchangeserver |Where-Object Name -ne $Exch| Get-Random).fqdn -Confirm:$false -ErrorAction SilentlyContinue
+        Suspend-ClusterNode -Name $Exch -ErrorAction silentlycontinue
+        Set-MailboxServer $Exch -DatabaseCopyActivationDisabledAndMoveNow $true
+        Set-MailboxServer $Exch -DatabaseCopyAutoActivationPolicy Blocked
+        while (Get-MailboxDatabaseCopyStatus -Server $Exch| Where-Object status -eq "mounted"){Move-ActiveMailboxDatabase -Server $Exch -Confirm:$false}
         ""
-        Write-Host "$env:COMPUTERNAME ComponentState HubTransport is" (Get-ServerComponentState $env:COMPUTERNAME -Component HubTransport).State "(Should Inactive)"
-        Write-Host "$env:COMPUTERNAME ComponentState ServerWideOffline is" (Get-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline).state "(Should InActive)" 
-        Write-Host "$env:COMPUTERNAME DatabaseCopyActivationDisabledAndMoveNow is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyActivationDisabledAndMoveNow "(Should True)"
-        Write-Host "$env:COMPUTERNAME DatabaseCopyAutoActivationPolicy is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyAutoActivationPolicy "(Should Blocked)"
+        Write-Host "$Exch ComponentState HubTransport is" (Get-ServerComponentState $Exch -Component HubTransport).State "(Should Inactive)"
+        Write-Host "$Exch ComponentState ServerWideOffline is" (Get-ServerComponentState $Exch -Component ServerWideOffline).state "(Should InActive)" 
+        Write-Host "$Exch DatabaseCopyActivationDisabledAndMoveNow is" (Get-MailboxServer $Exch).DatabaseCopyActivationDisabledAndMoveNow "(Should True)"
+        Write-Host "$Exch DatabaseCopyAutoActivationPolicy is" (Get-MailboxServer $Exch).DatabaseCopyAutoActivationPolicy "(Should Blocked)"
         ""
-        if ($DB=(Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME | Where-Object Status -eq "Mounted").name){
+        if ($DB=(Get-MailboxDatabaseCopyStatus -Server $Exch | Where-Object Status -eq "Mounted").name){
             Write-Host "You Have Some Bases Mounted!!! $DB" -ForeGround RED
         }
-        Else {
-            Write-Host "You Have No Bases Mounted!!!" -ForeGround GREEN
-        }
+        Else {Write-Host "You Have No Bases Mounted!!!" -ForeGround GREEN }
     }
     
     if ($Stop){
-        $Missed=Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME  |Where-Object {($_.Status -ne "Mounted") -and ($_.ActivationPreference -eq 1)}
         Write-Host "Prepearing to activate server components" -Foreground GREEN
-        Set-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline -State Active -Requester Maintenance
-        Resume-ClusterNode -Name $env:COMPUTERNAME -ErrorAction silentlycontinue
-        Set-MailboxServer $env:COMPUTERNAME -DatabaseCopyAutoActivationPolicy Unrestricted
-        Set-MailboxServer $env:COMPUTERNAME -DatabaseCopyActivationDisabledAndMoveNow $false
-        Set-ServerComponentState $env:COMPUTERNAME -Component HubTransport -State Active -Requester Maintenance
+        Set-ServerComponentState $Exch -Component ServerWideOffline -State Active -Requester Maintenance
+        Resume-ClusterNode -Name $Exch -ErrorAction silentlycontinue
+        Set-MailboxServer $Exch -DatabaseCopyAutoActivationPolicy Unrestricted
+        Set-MailboxServer $Exch -DatabaseCopyActivationDisabledAndMoveNow $false
+        Set-ServerComponentState $Exch -Component HubTransport -State Active -Requester Maintenance
         Start-Sleep 5
         ""
-        Write-Host "$env:COMPUTERNAME ComponentState HubTransport is" (Get-ServerComponentState $env:COMPUTERNAME -Component HubTransport).State "(Should Active)"
-        Write-Host "$env:COMPUTERNAME ComponentState ServerWideOffline is" (Get-ServerComponentState $env:COMPUTERNAME -Component ServerWideOffline).state "(Should Active)"
-        Write-Host "$env:COMPUTERNAME DatabaseCopyActivationDisabledAndMoveNow is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyActivationDisabledAndMoveNow "(Should False)"
-        Write-Host "$env:COMPUTERNAME DatabaseCopyAutoActivationPolicy is" (Get-MailboxServer $env:COMPUTERNAME).DatabaseCopyAutoActivationPolicy "(Should Unrestricted)"
+        Write-Host "$Exch ComponentState HubTransport is" (Get-ServerComponentState $Exch -Component HubTransport).State "(Should Active)"
+        Write-Host "$Exch ComponentState ServerWideOffline is" (Get-ServerComponentState $Exch -Component ServerWideOffline).state "(Should Active)"
+        Write-Host "$Exch DatabaseCopyActivationDisabledAndMoveNow is" (Get-MailboxServer $Exch).DatabaseCopyActivationDisabledAndMoveNow "(Should False)"
+        Write-Host "$Exch DatabaseCopyAutoActivationPolicy is" (Get-MailboxServer $Exch).DatabaseCopyAutoActivationPolicy "(Should Unrestricted)"
+        $Missed=Get-MailboxDatabaseCopyStatus -Server $Exch  |Where-Object {($_.Status -ne "Mounted") -and ($_.ActivationPreference -eq 1)}
         if ($Missed){
-            Get-MailboxDatabaseCopyStatus * -Active | Format-Table Name, Status, MailboxServer,ActivationPreference
+            $Missed| Format-Table Name, Status, MailboxServer,ActiveDatabaseCopy
             Write-Host "Prepearing to switch back Active Databases" -Foreground GREEN
             Start-Sleep 5
             while (
-                $Queue=Get-MailboxDatabaseCopyStatus -Server $env:COMPUTERNAME  |Where-Object ReplayQueueLength -gt 10){
+                $Queue=$Missed|Get-MailboxDatabaseCopyStatus |Where-Object ReplayQueueLength -gt 10){
                     Write-Host "Waiting for ReplayQueueLength" -ForegroundColor Yellow
                     $Queue| Select-Object Name, ReplayQueueLength
                     Start-Sleep 10
                 }
-            foreach ($mbx in $Missed.DatabaseName){
-                Move-ActiveMailboxDatabase -SkipAllChecks -Identity $mbx -ActivateOnServer $env:COMPUTERNAME -Confirm:$false
-            }
+            foreach ($mbx in $Missed.DatabaseName){Move-ActiveMailboxDatabase -SkipAllChecks -Identity $mbx -ActivateOnServer $Exch -Confirm:$false}
         }
-        Get-MailboxDatabaseCopyStatus * -Active | Format-Table Name, Status, MailboxServer, ActivationPreference
+        Get-MailboxDatabaseCopyStatus -Active | Format-Table Name, Status, MailboxServer,ActiveDatabaseCopy, ActivationPreference
     }
 }
